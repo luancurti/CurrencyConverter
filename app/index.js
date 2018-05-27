56da2d727843a6345d31ce8b798a828173acdbf9
luancurti <luancurti@gmail.com>
2018-05-27 19:29:33 -0300

    Add home screen

--- app/index.js
+++ app/index.js
@@ -1,19 +1,11 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import EStyleSheet from 'react-native-extended-stylesheet';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+import Home from './screens/Home';
+
+EStyleSheet.build({
+  $white: '#FFFFFF',
+  $primaryBlue: '#4F6D7A',
 });
 
-const App = () => (
-  <View style={styles.container}>
-    <Text>{"Let's Build Currency Converter"}</Text>
-  </View>
-);
-
-export default App;
+export default () => <Home />;
